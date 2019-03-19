46b00d90fd117cac3ccca3e3f4d98596e582a9a1
Zachary Lehmann <zlehmann@gmail.com>
2019-03-19 13:10:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function BoardMember (name, homeState, training) {
   this.name = name
-  this.homeState = homeState 
-  this.training = training 
+  this.homeState = homeState
+  this.training = training
   this.veto = function () { return "No, I must disagree" }
   this.approve = function () { return "You can do that!" }
   this.doCharity = function () { return "I like to help people." }
